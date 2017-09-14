448befba5eba4d8e72d2e15a133440e6e84ce612
cisen <csacxc@gmail.com>
2017-09-14 15:17:15 +0800

    feat: add new componet

---
+++ yymobile/textarea-item/index.tsx
@@ -0,0 +1,33 @@
+/* tslint:disable:jsx-no-multiline-js */
+import React from 'react';
+import TextareaItemProps from './PropsType';
+import { TextareaItem as _TextareaItem } from "antd-mobile";
+
+function noop() {}
+
+export interface TextareaItemState {
+  focus?: boolean;
+}
+
+export default class TextareaItem extends React.Component<TextareaItemProps, TextareaItemState> {
+  static defaultProps = {
+    prefixCls: 'yy-textarea',
+    prefixListCls: 'yy-list',
+    autoHeight: false,
+    editable: true,
+    disabled: false,
+    placeholder: '',
+    clear: false,
+    rows: 1,
+    onChange: noop,
+    onBlur: noop,
+    onFocus: noop,
+    onErrorClick: noop,
+    error: false,
+    labelNumber: 5,
+  };
+
+  render() {
+    return <TextareaItem {...this.props} />;
+  }
+}
