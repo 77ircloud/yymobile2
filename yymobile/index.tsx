a65700512fd70436d499336c42c46ae1ecad9ab6
肖唯 <xiaowei@77ircloud.com>
2017-11-15 16:54:53 +0800

    feat: 添加 YyCarousel  ,将指示器从圆点替换成数字

--- yymobile/index.tsx
+++ yymobile/index.tsx
@@ -75,3 +75,4 @@ export { default as YyButton } from './yy-button/index';
 export { default as YyPullLoad } from './yy-pull-load/index';
 export { default as YySearchBar } from './yy-search-bar/index';
 export { default as YyUpload } from './yy-upload/index';
+export { default as YyCarousel } from './yy-carousel/index';
