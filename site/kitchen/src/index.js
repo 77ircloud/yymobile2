97ae241aa53f82887d7769506f0fabfba02557f1
cisen <csacxc@gmail.com>
2017-12-06 10:34:52 +0800

    fix(bishegn): 修正需要打包安装问题，主要是要匹配最新的bisheng-plugin-antd接口

--- site/kitchen/src/index.js
+++ site/kitchen/src/index.js
@@ -5,7 +5,7 @@ const _indexDemos = ['drawer'];
 const _subListDemos = ['list-view'];
 
 const pluginAntdConfig = {
-  babelConfig: {
+  babelConfig: JSON.stringify({
     plugins: [
       [
         require.resolve('babel-plugin-import'), {
@@ -20,7 +20,7 @@ const pluginAntdConfig = {
         },
       ],
     ],
-  },
+  }),
 };
 
 module.exports = {
